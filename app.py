96e255bcc67f15c09c31e529ae2c31bfc27783f0
Ariel Bridgeman <ariel.bridgeman@gmail.com>
2018-12-05 17:36:26 +0100

    testing

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+import pandas as pd
+import plotly.graph_objs as go
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.read_csv('data.csv')
 
 server = app.server
 
