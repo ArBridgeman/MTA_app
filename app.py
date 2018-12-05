1341181a81d475d6e7c78e64c1505e9dd90decf5
Ariel Bridgeman <ariel.bridgeman@gmail.com>
2018-12-05 17:17:14 +0100

    testing dash app in heroku

--- app.py
+++ app.py
@@ -1,127 +1,179 @@
-# Load the packages
-
-from flask import Flask, render_template, request
-
-# from bokeh.embed import components
-from bokeh.models import HoverTool
-from bokeh.plotting import figure
-
-import numpy as np
-
-# Connect the app
-app = Flask(__name__)
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
 
 
-# def get_plot(df, title, form):
-#     """Create plot based on what checked or not"""
+app = dash.Dash()
 
-#     p1 = figure(x_axis_type="datetime", width=700, height=500,
-#                 sizing_mode='scale_both', title="Stock Prices for %s" % title)
+df = pd.read_csv('worldwide_indicators_by_year.csv')
 
-#     if request.form.get('adj_open'):
-#         if request.form.get('open'):
-#             p1.circle(df.index, df['Adj. Open'], color="#e74c3c", size=10,
-#                       legend='Adj. open', alpha=0.3)
-#         else:
-#             p1.line(df.index, df['Adj. Open'], color="#e74c3c", line_width=3,
-#                     legend='Adj. open')
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
 
-#     if request.form.get('adj_close'):
-#         if request.form.get('close'):
-#             p1.circle(df.index, df['Adj. Close'], color="#2ecc71", size=10,
-#                       legend='Adj. close', alpha=0.3)
-#         else:
-#             p1.line(df.index, df['Adj. Close'], color="#2ecc71", line_width=3,
-#                     legend='Adj. close')
+available_indicators = df['Indicator Name'].unique()
 
-#     if request.form.get('open'):
-#         p1.line(df.index, df['Open'], color="#9b59b6", line_width=3,
-#                 legend='Open')
+app.layout = html.Div([
 
-#     if request.form.get('close'):
-#         p1.line(df.index, df['Close'], color="#3498db", line_width=3,
-#                 legend='Close')
+    html.H1(
+        children='Worldwide indicators',
+        style={
+            'textAlign': 'center',
+            #'color': colors['text']
+        }
+    ),
+    html.Div([
 
-#     p1.xaxis.axis_label = 'Date'
-#     p1.xaxis.axis_label_text_font_size = "20pt"
-#     p1.xaxis.major_label_text_font_size = "16pt"
+        html.Div([
+            dcc.Dropdown(
+                id='crossfilter-xaxis-column',
+                options=[{'label': i, 'value': i}
+                         for i in available_indicators],
+                value='Electric power consumption (kWh per capita)'
+            ),
+            dcc.RadioItems(
+                id='crossfilter-xaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )], style={'width': '49%', 'display': 'inline-block'}
+        ),
 
-#     p1.yaxis.axis_label = 'Price'
-#     p1.yaxis.axis_label_text_font_size = "20pt"
-#     p1.yaxis.major_label_text_font_size = "16pt"
+        html.Div([
+            dcc.Dropdown(
+                id='crossfilter-yaxis-column',
+                options=[{'label': i, 'value': i}
+                         for i in available_indicators],
+                value='CO2 emissions (metric tons per capita)'
+            ),
+            dcc.RadioItems(
+                id='crossfilter-yaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )], style={'width': '49%', 'float': 'right', 'display': 'inline-block'}
+        )
+    ], style={
+        'borderBottom': 'thin lightgrey solid',
+        'backgroundColor': 'rgb(250, 250, 250)',
+        'padding': '10px 5px'
+    }),
 
-#     p1.title.text_font_size = '30pt'
-#     p1.legend.label_text_font_size = "16pt"
-#     p1.legend.location = "bottom_right"
 
-#     p1.add_tools(HoverTool())
-#     return(p1)
+    html.Div([
+        dcc.Graph(
+            id='crossfilter-indicator-scatter',
+            hoverData={'points': [{'customdata': 'Norway'}]}
+        )], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}
+    ),
+
+    html.Div([
+        dcc.Graph(id='y-time-series'),
+        dcc.Graph(id='x-time-series')
+    ], style={'width': '49%', 'padding-left': '25px', 'display': 'inline-block'}
+    ),
+
+    html.Div(
+        dcc.Slider(
+            id='crossfilter-year--slider',
+            min=df['Year'].min(),
+            max=df['Year'].max(),
+            value=df['Year'].max(),
+            marks={str(year): str(year) for year in df['Year'].unique()}
+        ), style={'width': '48%', 'padding': '0px 20px 20px 20px'}
+    )
+])
 
 
-@app.route('/')
-def homepage():
-    """Render home page"""
-    return render_template('home.html')
+@app.callback(
+    dash.dependencies.Output('crossfilter-indicator-scatter', 'figure'),
+    [dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
+     dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
+     dash.dependencies.Input('crossfilter-xaxis-type', 'value'),
+     dash.dependencies.Input('crossfilter-yaxis-type', 'value'),
+     dash.dependencies.Input('crossfilter-year--slider', 'value')])
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type, year_value):
+
+    dff = df[df['Year'] == year_value]
+
+    return {
+        'data': [go.Scatter(
+            x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
+            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
+            text=dff[dff['Indicator Name'] ==
+                     yaxis_column_name]['Country Name'],
+            customdata=dff[dff['Indicator Name'] ==
+                           yaxis_column_name]['Country Name'],
+            mode='markers',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': xaxis_column_name,
+                'type': 'linear' if xaxis_type == 'Linear' else 'log'
+            },
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear' if yaxis_type == 'Linear' else 'log'
+            },
+            margin={'l': 40, 'b': 30, 't': 10, 'r': 0},
+            height=600,
+            hovermode='closest'
+        )
+    }
 
 
-@app.route('/graph', methods=["GET", "POST"])
-def graph():
-    #     # script, div = "", ""
-    #     # ticker = request.form['ticker']
-    #     # search = ""
-    #     # ticks = ""
+def create_time_series(dff, axis_type, title):
+    return {
+        'data': [go.Scatter(
+            x=dff['Year'],
+            y=dff['Value'],
+            mode='lines+markers',
+            marker={'color': '#fdaa48'},
+        )],
+        'layout': {
+            'title': title,
+            'height': 300,  # 225
+            #'width': 500,
+            'titlefont': {'size': 14},
+            'margin': {'l': 35, 'b': 45, 'r': 10, 't': 25},  # 't': 10 'b': 40
+            'yaxis': {'type': 'linear' if axis_type == 'Linear' else 'log'},
+            'xaxis': {'showgrid': False}
+        }
+    }
 
-    #     # try:
-    #     #     data = quandl.get("WIKI/%s" % ticker)
-    #     #     message = ""
-    #     #     ticks = ticker
-    #     #     search = "https://www.google.com/search?q=%s" % (ticker)
 
-    #     #     oldest, newest = data.index.min(), data.index.max()
-    #     #     old_spec = np.datetime64(request.form["start"])
-    #     #     new_spec = np.datetime64(request.form["end"])
+@app.callback(
+    dash.dependencies.Output('y-time-series', 'figure'),
+    [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
+     dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
+     dash.dependencies.Input('crossfilter-yaxis-type', 'value')])
+def update_y_timeseries(hoverData, yaxis_column_name, axis_type):
+    country_name = hoverData['points'][0]['customdata']
+    dff = df[df['Country Name'] == country_name]
+    dff = dff[dff['Indicator Name'] == yaxis_column_name]
+    title = '<b>{}</b><br>{}'.format(country_name, yaxis_column_name)
+    return create_time_series(dff, axis_type, title)
 
-    #     #     if new_spec <= old_spec:
-    #     #         message = "The supplied date range was given incorrectly. \
-    #     #         Start date (%s) must be a later date than end date(%s). \
-    #     #         Please submit a new query." % (old_spec, new_spec)
-    #     #         return render_template('graph.html', message=message,
-    #     #                                script=script, div=div)
 
-    #     #     mask = (data.index >= old_spec) & (data.index <= new_spec)
-    #     #     data = data[mask]
+@app.callback(
+    dash.dependencies.Output('x-time-series', 'figure'),
+    [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
+     dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
+     dash.dependencies.Input('crossfilter-xaxis-type', 'value')])
+def update_y_timeseries(hoverData, xaxis_column_name, axis_type):
+    dff = df[df['Country Name'] == hoverData['points'][0]['customdata']]
+    dff = dff[dff['Indicator Name'] == xaxis_column_name]
+    return create_time_series(dff, axis_type, xaxis_column_name)
 
-    #     #     dt = (new_spec - old_spec)
-    #     #     option = bool(int(request.form["yes_no"]))
-
-    #     #     if dt > np.timedelta64(2 * 365, 'D') and dt <= np.timedelta64(3 * 365, 'D')\
-    #     #             and option:
-    #     #         data = data.resample('W').mean()
-    #     #     if dt > np.timedelta64(3 * 365, 'D') and option:
-    #     #         data = data.resample('M').mean()
-
-    #     #     if data.shape[0] > 0:
-    #     #         # create plot, as have data
-    #     #         p = get_plot(data, ticker, request)
-    #     #         script, div = components(p)
-
-    #     #         if old_spec < np.datetime64(oldest):
-    #     #             message = "Please note that the supplied data range was %s to %s. \
-    #     #         The data was available in a subset of this range from %s to %s" % (
-    #     #                 old_spec, new_spec, np.datetime64(oldest, 'D'),
-    #     #                 np.datetime64(newest, 'D'))
-    #     #     else:
-    #     #         message = "%s exists but no data in chosen date range of %s to %s.\
-    #     #          \nPlease select something between %s to %s." % (
-    #     #             ticker, old_spec, new_spec, oldest, newest)
-    #     # except NotFoundError:
-    #     #     message = "%s is not found in the Quandl/WIKI API" % ticker
-    #     # except:
-    #     #     message = "Undefined error associated with implementation."
-
-    #     # return render_template('graph.html', message=message, ticker=ticks,
-    #     #                        search=search, script=script, div=div)
-    return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Set to false when deploying
+    app.run_server()
